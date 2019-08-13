2f36221573829b878f6d51645e1e869b193e2f6a
Bryan Mayo <bryan@digonsystems.com>
2019-08-13 09:27:53 -0600

    added delegate practice class

---
+++ TestingStuff/DelegatePractice.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingStuff
+{
+    public delegate string MessageResponsePayloadPolicy(MessagePayload payload);
+
+    public interface IDelegatePractice
+    {
+        void SetPolicy(MessageResponsePayloadPolicy payloadPolicy);
+        MessageResponse ReponsePayload(int[] values);
+    }
+
+    public class DelegatePractice
+    {
+        MessageResponsePayloadPolicy policy;
+
+        public DelegatePractice()
+        {
+            policy = MessagePayloadReponsePolicies.DefaultMessageResponsePayloadPolicy;
+        }
+
+        public void SetPolicy(MessageResponsePayloadPolicy payloadPolicy)
+        {
+            policy = payloadPolicy;
+        }
+
+        public MessageResponse ReponsePayload(int[] values)
+        {
+            var payload = new MessagePayload() { Values = values };
+
+            var response = new MessageResponse
+            {
+                Payload = policy(payload)
+            };
+
+            return response;
+        }
+
+    }
+
+    public static class MessagePayloadReponsePolicies
+    {
+        public static string DefaultMessageResponsePayloadPolicy(MessagePayload payload)
+        {
+            return "-";
+        }
+
+        public static string StringMessageResponsePayloadPolicy(MessagePayload payload)
+        {
+            return "{"+ string.Join(":", payload.Values) +"}";
+        }
+
+        public static string JsonMessageResponsePayloadPolicy(MessagePayload payload)
+        {
+            return JsonConvert.SerializeObject(payload.Values);
+        }
+    }
+
+    public class MessagePayload
+    {
+        public int[] Values { get; set; }
+
+    }
+
+    public class MessageResponse
+    {
+        public string Payload { get; set; }
+    }
+
+}
---
+++ TestingStuffTests/TestDelegatePractice.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingStuff;
+
+namespace TestingStuffTests
+{
+    [TestClass]
+    public class TestDelegatePractice
+    {
+        [TestMethod]
+        public void TestMessagePayloadString()
+        {
+            var delegatePractice = new DelegatePractice();
+            delegatePractice.SetPolicy(MessagePayloadReponsePolicies.StringMessageResponsePayloadPolicy);
+
+            var result = delegatePractice.ReponsePayload(new int[] { 1, 2, 3, 4 });
+
+            Assert.IsTrue(result.Payload == "{1:2:3:4}");
+        }
+
+        [TestMethod]
+        public void TestMessagePayloadJson()
+        {
+            var delegatePractice = new DelegatePractice();
+            delegatePractice.SetPolicy(MessagePayloadReponsePolicies.JsonMessageResponsePayloadPolicy);
+
+            var result = delegatePractice.ReponsePayload(new int[] { 1, 2, 3, 4 });
+
+            Assert.IsTrue(result.Payload == "[1,2,3,4]");
+
+        }
+    }
+}
