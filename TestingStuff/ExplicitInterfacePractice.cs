dd047faa2c15033597ebc3e39a363de9ae8e53c8
Bryan Mayo <bryan@digonsystems.com>
2019-08-15 08:55:06 -0600

    explicit interface practice with tests

---
+++ TestingStuff/ExplicitInterfacePractice.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingStuff
+{
+    public class ExplicitInterfacePractice
+    {
+        public string GetReponse()
+        {
+            IBaseReponse proxy = new ProxyReponseStub();
+
+            return proxy.GetReponse();
+        }
+
+        public string GetAlphaReponse()
+        {
+            IAlphaReponse proxy = new ProxyReponseStub();
+
+            return proxy.GetReponse();
+        }
+
+        public string GetBetaReponse()
+        {
+            IBetaReponse proxy = new ProxyReponseStub();
+
+            return proxy.GetReponse();
+        }
+
+        public string GetReponse<T>() where T  : IBaseReponse
+        {
+            var proxy = new ProxyReponseStub();
+
+            return proxy.GetReponse<T>();
+        }
+    }
+
+    
+
+    public interface IBaseReponse
+    {
+        string GetReponse();
+    }
+
+
+
+    public interface IAlphaReponse : IBaseReponse
+    {
+        new string GetReponse();
+    }
+
+    public interface IBetaReponse: IBaseReponse
+    {
+        new string GetReponse();
+    }
+
+    public class ProxyReponseStub : IAlphaReponse, IBetaReponse
+    {
+        public string GetReponse<T>()
+        {
+            if(typeof(T) == typeof(IAlphaReponse))
+            {
+                return ((IAlphaReponse)this).GetReponse();
+            }
+            else if(typeof(T) == typeof(IBetaReponse))
+            {
+                return ((IBetaReponse)this).GetReponse();
+            }
+
+            return GetReponse();
+        }
+
+        public string GetReponse()
+        {
+            return "omega";
+        }
+
+        string IAlphaReponse.GetReponse()
+        {
+            return "alpha";
+        }
+
+        string IBetaReponse.GetReponse()
+        {
+            return "beta";
+        }
+    }
+}
---
+++ TestingStuffTests/TestExplicitInterfacePractice.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingStuff;
+
+namespace TestingStuffTests
+{
+    [TestClass]
+    public class TestExplicitInterfacePractice
+    {
+        [TestMethod]
+        public void GetReponse(){
+            var explicitInterface = new ExplicitInterfacePractice();
+
+            var result = explicitInterface.GetReponse();
+
+            Assert.IsTrue(result == "omega");
+        }
+
+        [TestMethod]
+        public void GetAlphaReponse()
+        {
+            var explicitInterface = new ExplicitInterfacePractice();
+
+            var result = explicitInterface.GetAlphaReponse();
+
+            Assert.IsTrue(result == "alpha");
+        }
+
+        [TestMethod]
+        public void GetBravoReponse()
+        {
+            var explicitInterface = new ExplicitInterfacePractice();
+
+            var result = explicitInterface.GetBetaReponse();
+
+            Assert.IsTrue(result == "beta");
+        }
+
+        [TestMethod]
+        public void GetGenericReponse()
+        {
+            var explicitInterface = new ExplicitInterfacePractice();
+
+            var result = explicitInterface.GetReponse<IAlphaReponse>();
+
+            Assert.IsTrue(result == "alpha");
+        }
+
+
+        [TestMethod]
+        public void GetUnknownGenericReponse()
+        {
+            var explicitInterface = new ExplicitInterfacePractice();
+
+            var result = explicitInterface.GetReponse<IDunnoResponse>();
+
+            Assert.IsTrue(result == "omega");
+        }
+
+        public interface IDunnoResponse: IBaseReponse
+        {
+
+        }
+
+
+    }
+}
